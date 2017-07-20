54e697f66f164dafff322d87624cff00d62ba1d3
Tyler D. Franklin <irishlep84@gmail.com>
2017-07-20 14:06:45 -0500

    Completed Checkpoint 2

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,4 @@
-require 'HTTParty'
+require 'httparty'
 
 class Kele
     include HTTParty
@@ -6,7 +6,7 @@ class Kele
     BASE_URI = 'https://www.bloc.io/api/v1'
 
     def initialize(email, password)
-        response = self.class.post('#{BASE_URI}/sessions', body: { 'email': email, 'password': password })
+        response = self.class.post("#{BASE_URI}/sessions", body: { 'email': email, 'password': password })
         raise "Invalid email or password" if response.code == 401
         @auth = response['auth_token']
     end
